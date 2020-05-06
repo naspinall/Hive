f08b4254eefeb4a7a7822d5fcde9a33bed6b38cc
Nick Aspinall <nickhaspinall@gmail.com>
2020-05-06 22:53:48 +0930

    Forgot to add a file :(

---
+++ pkg/config/config.go
@@ -0,0 +1,75 @@
+package config
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type PostgresConfig struct {
+	Host     string `json:"host"`
+	Port     int    `json:"port"`
+	User     string `json:"user"`
+	Password string `json:"password"`
+	Name     string `json:"name"`
+}
+
+type Config struct {
+	Port     int            `json:"port"`
+	Env      string         `json:"env"`
+	Pepper   string         `json:"pepper"`
+	JWTKey   string         `json:"jwtKey"`
+	Database PostgresConfig `json:"database"`
+}
+
+func (c PostgresConfig) Dialect() string {
+	return "postgres"
+}
+
+func (c PostgresConfig) ConnectionInfo() string {
+	if c.Password == "" {
+		return fmt.Sprintf("host=%s port=%d user=%s dbname=%s sslmode=disable", c.Host, c.Port, c.User, c.Name)
+	}
+
+	return fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", c.Host, c.Port, c.User, c.Password, c.Name)
+}
+
+func DefaultPostgresConfig() PostgresConfig {
+	return PostgresConfig{
+		Host:     "localhost",
+		Port:     5432,
+		User:     "postgres",
+		Password: "hive",
+		Name:     "hive",
+	}
+}
+
+func (c Config) IsProd() bool {
+	return c.Env == "production"
+}
+
+func DefaultConfig() Config {
+	return Config{
+		Port:     3001,
+		Env:      "development",
+		Pepper:   "salt-and-pepper-is-delicious",
+		JWTKey:   "jwt-make-life-easy",
+		Database: DefaultPostgresConfig(),
+	}
+}
+
+func LoadConfig() (c Config) {
+	f, err := os.Open("./config.json")
+	if err != nil {
+		fmt.Println("No config file found, using default config")
+		return DefaultConfig()
+	}
+
+	err = json.NewDecoder(f).Decode(&c)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Loaded config")
+	return
+}
